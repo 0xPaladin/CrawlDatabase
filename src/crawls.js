516e19b9b24ca2c79c1442fd0e172da14e33b20b
0xPaladin <15690809+0xPaladin@users.noreply.github.com>
2022-11-09 13:09:32 -0600

    V1.1
    Added resources from reddit source: https://www.reddit.com/r/osr/comments/rtzam6/hexcrawl_resources_i_have_known_and_loved/

--- src/crawls.js
+++ src/crawls.js
@@ -1,85 +1,189 @@
-let CRAWLS = [
-  {
-    "title" : "Vaults of Varn",
-    "links" : {
-      "blog" : "https://vaultsofvaarn.com/",
-      "itch" : "https://graculusdroog.itch.io/",
-      "SRD" : "https://vaarn.github.io/#/",
-      "Patreon" : "https://www.patreon.com/vaultsofvaarn"
-    },
-    "systems" : "OSR,Knave",
-    "tags": "setting,weird,desert,point"
+let CRAWLS = [{
+  "title": "Vaults of Varn",
+  "links": {
+    "blog": "https://vaultsofvaarn.com/",
+    "itch": "https://graculusdroog.itch.io/",
+    "SRD": "https://vaarn.github.io/#/",
+    "Patreon": "https://www.patreon.com/vaultsofvaarn"
   },
-  {
-    "title" : "Vast in the Dark",
-    "links" : {
-      "itch" : "https://feral-indie-studios.itch.io/vast-in-the-dark",
-    },
-    "systems" : "OSR",
-    "tags": "setting,weird,brutal,desert,hex"
+  "systems": "OSR,Knave",
+  "tags": "setting,weird,desert,point"
+}, {
+  "title": "Vast in the Dark",
+  "links": {
+    "itch": "https://feral-indie-studios.itch.io/vast-in-the-dark",
   },
-  {
-    "title" : "Downcrawl",
-    "links" : {
-      "blog" : "https://aaronareed.net/",
-      "itch" : "https://aareed.itch.io/downcrawl",
-      "drivethru" : "https://www.drivethrurpg.com/product/278571/Downcrawl"
-    },
-    "systems" : "AW",
-    "tags": "setting,underground,point"
+  "systems": "OSR",
+  "tags": "setting,weird,brutal,desert,hex"
+}, {
+  "title": "Downcrawl",
+  "links": {
+    "blog": "https://aaronareed.net/",
+    "itch": "https://aareed.itch.io/downcrawl",
+    "drivethru": "https://www.drivethrurpg.com/product/278571/Downcrawl"
   },
-  {
-    "title" : "Skycrawl",
-    "links" : {
-      "blog" : "https://aaronareed.net/",
-      "drivethru" : "https://www.drivethrurpg.com/product/328583/Skycrawl"
-    },
-    "systems" : "AW",
-    "tags": "setting,sky,point"
+  "systems": "AW",
+  "tags": "setting,underground,point"
+}, {
+  "title": "Skycrawl",
+  "links": {
+    "blog": "https://aaronareed.net/",
+    "drivethru": "https://www.drivethrurpg.com/product/328583/Skycrawl"
   },
-  {
-    "title" : "Ultraviolet Grasslands",
-    "links" : {
-      "blog" : "https://www.wizardthieffighter.com/",
-      "itch" : "https://wizardthieffighter.itch.io/ultraviolet-grasslands-and-the-black-city-uvg",
-      "patreon" : "http://patreon.com/wizardthieffighter"
-    },
-    "systems" : "OSR,unique",
-    "tags": "setting,weird,plains,point"
+  "systems": "AW",
+  "tags": "setting,sky,point"
+}, {
+  "title": "Ultraviolet Grasslands",
+  "links": {
+    "blog": "https://www.wizardthieffighter.com/",
+    "itch": "https://wizardthieffighter.itch.io/ultraviolet-grasslands-and-the-black-city-uvg",
+    "patreon": "http://patreon.com/wizardthieffighter"
   },
-  {
-    "title" : "Cepheus Engine",
-    "links" : {
-      "SRD" : "https://www.orffenspace.com/cepheus-srd/",
-      "drivethru" : "https://www.drivethrurpg.com/product/186894/Cepheus-Engine-System-Reference-Document"
-    },
-    "systems" : "Traveller",
-    "tags": "setting,scifi,toolbox,hex"
+  "systems": "OSR,unique",
+  "tags": "setting,weird,plains,point"
+}, {
+  "title": "Cepheus Engine",
+  "links": {
+    "SRD": "https://www.orffenspace.com/cepheus-srd/",
+    "drivethru": "https://www.drivethrurpg.com/product/186894/Cepheus-Engine-System-Reference-Document"
   },
-  {
-    "title" : "Stars Without Number",
-    "links" : {
-      "drivethru" : "https://www.drivethrurpg.com/product/226996/Stars-Without-Number-Revised-Edition"
-    },
-    "systems" : "OSR",
-    "tags": "setting,scifi,toolbox,hex"
+  "systems": "Traveller",
+  "tags": "setting,scifi,toolbox,hex"
+}, {
+  "title": "Stars Without Number",
+  "links": {
+    "drivethru": "https://www.drivethrurpg.com/product/226996/Stars-Without-Number-Revised-Edition"
   },
-  {
-    "title" : "The Perilous Wilds",
-    "links" : {
-      "drivethru" : "https://www.drivethrurpg.com/product/156979/The-Perilous-Wilds"
-    },
-    "systems" : "AW,DW",
-    "tags": "fantasy,toolbox,point"
+  "systems": "OSR",
+  "tags": "setting,scifi,toolbox,hex"
+}, {
+  "title": "Worlds Without Number",
+  "links": {
+    "drivethru": "https://www.drivethrurpg.com/product/348809/Worlds-Without-Number-Free-Edition"
   },
-  {
-    "title" : "The Gardens Of Ynn",
-    "links" : {
-      "drivethru" : "https://www.drivethrurpg.com/product/237544/The-Gardens-Of-Ynn"
-    },
-    "systems" : "OSR",
-    "tags": "fantasy,point"
-  }
-]
+  "systems": "OSR",
+  "tags": "setting,fantasy,toolbox,point"
+}, {
+  "title": "Perilous Wilds",
+  "links": {
+    "drivethru": "https://www.drivethrurpg.com/product/156979/The-Perilous-Wilds"
+  },
+  "systems": "AW,DW",
+  "tags": "fantasy,toolbox,point"
+}, {
+  "title": "Gardens Of Ynn",
+  "links": {
+    "drivethru": "https://www.drivethrurpg.com/product/237544/The-Gardens-Of-Ynn"
+  },
+  "systems": "OSR",
+  "tags": "fantasy,point"
+}, {
+  "title": "How to Hexcrawl (youtube)",
+  "links": {
+    "youtube": "https://www.youtube.com/watch?v=eC-h1haFSIA"
+  },
+  "systems": "D&D,OSR",
+  "tags": "howto"
+}, {
+  "title": "Wilderness Hexplore",
+  "links": {
+    "blog": "http://www.jedmc.com/ixdd/2016/7/12/wilderness-hexplore",
+    "pdf": "http://www.jedmc.com/s/Wilderness_Hexplore_revised_112913_us.pdf"
+  },
+  "systems": "generic",
+  "tags": "fantasy,hex"
+}, {
+  "title": "Manual of Hexterity",
+  "links": {
+    "drivethru": "https://www.drivethrurpg.com/m/product/395396",
+  },
+  "systems": "OSR,generic",
+  "tags": "fantasy,hex"
+}, {
+  "title": "100 Wilderness Hexes",
+  "links": {
+    "blog": "https://blog.d4caltrops.com/2018/03/wilderness-hexes-version-10.html",
+  },
+  "systems": "generic",
+  "tags": "fantasy,premade,hex"
+}, {
+  "title": "d4 Caltrops d100 Tables",
+  "links": {
+    "blog": "https://blog.d4caltrops.com/search/label/d100",
+  },
+  "systems": "generic",
+  "tags": "fantasy,generator,toolbox"
+}, {
+  "title": "Hex Flower Cookbook",
+  "links": {
+    "blog": "https://goblinshenchman.wordpress.com",
+    "drivethru": "https://www.drivethrurpg.com/product/295083/Hex-Flower-Cookbook--an-overview-and-some-thoughts-on-Hex-Flower-Game-Engines-by-Goblins-Henchman",
+  },
+  "systems": "generic",
+  "tags": "fantasy,toolbox,hex,hexflower"
+}, {
+  "title": "d30 Sandbox Companion",
+  "links": {
+    "drivethru": "https://www.drivethrurpg.com/product/124392/d30-Sandbox-Companion",
+  },
+  "systems": "generic,OSR",
+  "tags": "fantasy,toolbox,hex"
+}, {
+  "title": "d30 DM Companion",
+  "links": {
+    "drivethru": "https://www.drivethrurpg.com/product/102567/d30-DM-Companion",
+  },
+  "systems": "generic,OSR",
+  "tags": "fantasy,toolbox,hex"
+}, {
+  "title": "Veins of the Earth",
+  "links": {
+    "drivethru": "https://www.drivethrurpg.com/product/209509/Veins-of-the-Earth"
+  },
+  "systems": "generic,OSR",
+  "tags": "underground,point"
+},{
+  "title": "Hexcrawls by The Alexandrian",
+  "links": {
+    "blog": "https://thealexandrian.net/wordpress/17308/roleplaying-games/hexcrawl"
+  },
+  "systems": "generic",
+  "tags": "fantasy,toolbox,hex"
+} ,{
+  "title": "Welsh Piper's Hex-based Campaign Design",
+  "links": {
+    "blog": "https://welshpiper.com/hex-based-campaign-design-part-1/"
+  },
+  "systems": "generic",
+  "tags": "fantasy,toolbox,hex"
+},{
+  "title": "Scarlet Heroes",
+  "links": {
+    "drivethru": "https://www.drivethrurpg.com/product/127180/Scarlet-Heroes"
+  },
+  "systems": "OSR",
+  "tags": "fantasy,toolbox,solo,hex"
+},{
+  "title": "Unconquered",
+  "links": {
+    "itch": "https://monkeys-paw-games.itch.io/unconquered"
+  },
+  "systems": "OSR,ItO",
+  "tags": "fantasy,weird,setting,point"
+},{
+  "title": "Glide",
+  "links": {
+    "itch": "https://sleepysasquatch.itch.io/glide"
+  },
+  "systems": "unique,d6",
+  "tags": "scifi,setting,hex,desert,solo,boardgame"
+},{
+  "title": "Courier",
+  "links": {
+    "itch": "https://sleepysasquatch.itch.io/courier"
+  },
+  "systems": "unique,2d6",
+  "tags": "apoc,setting,hex,solo,boardgame"
+}]
 
 export {CRAWLS as default}
